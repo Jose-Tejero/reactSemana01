601e845169109a5f82a8f66694b069f7b8892d93
Jose-Tejero <j_jtejero@hotmail.com>
2021-12-23 10:09:10 -0600

    Listo animaciones

--- src/components/Author.js
+++ src/components/Author.js
@@ -4,12 +4,12 @@ const Author = ({author, changeQuote, color}) => {
 
     const styles = {
         background: color,
-        boxShadow: `0 0 10px #000`
+        boxShadow: `10px 10px 15px #000`
     }
 
     return (
         <div>
-            <h4>{author}</h4>
+            <h4 style={{color}} >{author}</h4>
             <button onClick={changeQuote} style={styles} >
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-chevrons-right" width="32" height="32" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
